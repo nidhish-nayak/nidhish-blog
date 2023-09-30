bca89322def9cbca2cde9f16c1cd987ce7deb081
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-09-30 20:36:56 +0530

    Added site link to configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nid-blog.vercel.app",
-  integrations: [tailwind(), sitemap(), prefetch()]
+	site: "https://nid-blog.vercel.app/",
+	integrations: [tailwind(), sitemap(), prefetch()],
 });
