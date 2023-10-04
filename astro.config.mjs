db28234a7f3f63f6faf0f6dbf77466c429d91993
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-10-04 17:33:28 +0530

    Removed analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
