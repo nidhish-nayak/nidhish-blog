0525cdbcb2582348f652c9dd50b06855e46b24b1
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-10-04 17:27:36 +0530

    Added vercel/analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://nid-blog.vercel.app/",
 	integrations: [tailwind(), sitemap(), prefetch()],
+	output: "server",
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
 });
