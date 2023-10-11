c8b919b4b296864ca9cf673ee4ea73cad11faf52
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-10-11 23:08:11 +0530

    Added preact islands, completed RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://nid-blog.vercel.app/",
-	integrations: [tailwind(), sitemap(), prefetch()],
+  site: "https://nid-blog.vercel.app/",
+  integrations: [tailwind(), sitemap(), prefetch(), preact()]
 });
