6d0d34d9d2e70ff096f2c783ea90643b328d0e74
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-10-12 17:39:36 +0530

    Updated tags page to reuse the Posts component

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,8 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nid-blog.vercel.app/",
-  integrations: [tailwind(), sitemap(), prefetch(), preact()]
+	site: "https://nid-blog.vercel.app/",
+	integrations: [tailwind(), sitemap(), prefetch()],
 });
