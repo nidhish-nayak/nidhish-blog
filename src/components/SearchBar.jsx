0afc9aa9b13b9a64a772bdfa1315c96fc616be8f
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-10-15 23:44:31 +0530

    Adding search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 const SearchBar = ({ props }) => {
 
   return (
-    <a href={props.url} class="bg-zinc-200 dark:bg-zinc-900 p-4 rounded-md group hover:shadow-lg">
+    <a href={props.url} class="bg-zinc-200 dark:bg-zinc-900 p-4 rounded-md group">
       <div class="relative">
         <span class="arrow absolute top-0 right-0 text-lg group-hover:text-orange-500 dark:group-hover:text-purple-500">&#129125;</span>
         <article class="flex flex-col gap-2">
@@ -9,7 +9,7 @@ const SearchBar = ({ props }) => {
           <p class="max-w-[300px]">{props.frontmatter.description}</p>
           <p class="flex flex-wrap gap-2">
             {props.frontmatter.tags.map(tag =>
-              <span class="bg-zinc-100 dark:bg-zinc-800 p-2 rounded-md">{tag}</span>)
+              <span class="bg-zinc-100 dark:bg-zinc-800 py-1 px-2 rounded-md">{tag}</span>)
             }
           </p>
         </article>
